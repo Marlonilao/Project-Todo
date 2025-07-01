2090a588869346d4bf71956b5dc4e395d46ad7c4
Marlonilao <176732593+Marlonilao@users.noreply.github.com>
2025-07-01 22:17:41 +0800

    wip

--- src/dom.js
+++ src/dom.js
@@ -1,11 +1,51 @@
-function renderProjectLists(ul){
+import Project from './logic.js';
+
+
+function renderProjectLists(ul,main){
     ul.innerHTML = '';
     const keys = Object.keys(localStorage);
     keys.forEach((key) => {
         const li = document.createElement('li');
-        li.innerText = `${key}`;
+        li.innerText = `${key}`
+        li.addEventListener('click',function(){
+            renderMainContent(main,key);
+        });
         ul.appendChild(li);
     })
 }
 
-export {renderProjectLists};
+function renderMainContent(main,key){
+    main.innerHTML = 
+    `<h2>${key}</h2>
+    <label><input type="text" placeholder="Add task"><button id="add-task-btn">Add</button></label>
+    <ul id="tasksList"> </ul>`;
+    const input = document.querySelector('input[placeholder="Add task"]');
+    const addTaskBtn = document.getElementById('add-task-btn');
+
+    const data = JSON.parse(localStorage.getItem(key));
+    const newObj = new Project(data.name)
+    newObj.task = data.task;
+    renderTaskList(newObj);
+
+    addTaskBtn.addEventListener('click',e=>{
+        e.preventDefault();
+        newObj.addTask(input.value);
+        localStorage.setItem(key,JSON.stringify(newObj));
+        input.value = ''
+        renderTaskList(newObj);
+    })
+}
+
+function renderTaskList(object) {
+    const ul = document.getElementById('tasksList');
+    ul.innerHTML = '';
+    object.getTasks().forEach(task => {
+        const li = document.createElement('li');
+        li.innerText = task;
+        ul.appendChild(li);
+    })
+}
+
+
+
+export {renderProjectLists, renderMainContent};
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './styles.css';
 import Project from './logic.js';
-import { renderProjectLists } from './dom.js';
+import { renderProjectLists, renderMainContent } from './dom.js';
 
-
+const main = document.getElementById('main')
 const projectsUl = document.getElementById('projects-ul')
 const inputProjectName = document.getElementById('project-name-input');
 const dialog = document.querySelector('body > dialog')
@@ -11,7 +11,7 @@ addProjectBtn.addEventListener('click', (e)=>{
     e.preventDefault();
     const projectObj = new Project(inputProjectName.value)
     localStorage.setItem(inputProjectName.value, JSON.stringify(projectObj));
-    renderProjectLists(projectsUl);
+    renderProjectLists(projectsUl, main);
     dialog.close();
     document.querySelector('body > dialog > form').reset();
 })
